c4e46ae26c2e74abde83d9a8b44629723d6b669e
Avishkar Autar <aautar@digital-radiation.com>
2016-06-24 20:51:47 -0400

    allowing setting server port from environment variable, logging of requests

--- echo.go
+++ echo.go
@@ -1,26 +1,40 @@
 package main
 
 import (
-
     "net/http"
+    "os"
+    "log"
 )
 
+// DefaultPort is the default port to use if once is not specified by the SERVER_PORT environment variable
+const DefaultPort = "7893";
+
 func getServerPort() (string) {
-    return "7893";
+    port := os.Getenv("SERVER_PORT");
+    if port != "" {
+        return port;
+    }
+
+    return DefaultPort;
 }
 
+// EchoHandler echos back the request as a response
 func EchoHandler(writer http.ResponseWriter, request *http.Request) {
+
+    log.Println("Echoing back request made to " + request.URL.Path + " to client (" + request.RemoteAddr + ")")
 
     writer.Header().Set("Access-Control-Allow-Origin", "*")
 
     // allow pre-flight headers
     writer.Header().Set("Access-Control-Allow-Headers", "Content-Range, Content-Disposition, Content-Type, ETag")
 
-
     request.Write(writer)
 }
 
 func main() {
+
+    log.Println("starting server, listening on port " + getServerPort())
+
     http.HandleFunc("/", EchoHandler)
     http.ListenAndServe(":" + getServerPort(), nil)
 }
